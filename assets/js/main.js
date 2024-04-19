f1d53eee358c63c85d473bc469f90a6c1ae0aa8b
gstgeorge <62919189+gstgeorge@users.noreply.github.com>
2024-04-18 21:49:19 -0500

    bootstrap scss colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,41 @@
-$(".lightbox, .gallery-item").each(function() {
-    $(this).on("click", function() {
-        $("#lightbox").modal("toggle");
-        $("#lightboxTitle").text($(this).attr("alt"));
-        $("#lightboxImage").attr("src", $(this).attr("src"));
+$('.lightbox, .gallery-item').each(function() {
+    $(this).on('click', function() {
+        $('#lightbox').modal('toggle');
+        $('#lightboxTitle').text($(this).attr('alt'));
+        $('#lightboxImage').attr('src', $(this).attr('src'));
     });
     $(this).css('cursor', 'pointer');
 });
 
-if ($("#jump-top").length) {
-    $(document).on("scroll", function() {
+if ($('#jump-top').length) {
+    $(document).on('scroll', function() {
         if ($(document).scrollTop() >= 500) {
-            $("#jump-top").removeClass("hidden");
+            $('#jump-top').removeClass('hidden');
         }
         else {
-            $("#jump-top").addClass("hidden");
+            $('#jump-top').addClass('hidden');
         }
     });
     
-    $("#jump-top").on("click", function() {
+    $('#jump-top').on('click', function() {
         $(document).scrollTop(0);
     });
 }
 
-$(".border-hover").hover(
+$('.dropdown-item').hover(
     function() {
-        $(this).addClass("selected");
+        $(this).addClass('text-bg-dark');
     }, 
     function() {
-        $(this).removeClass("selected");
+        $(this).removeClass('text-bg-dark');
+    }
+);
+
+$('.border-hover').hover(
+    function() {
+        $(this).addClass('selected');
+    }, 
+    function() {
+        $(this).removeClass('selected');
     }
 );
